9b2d59258cee7158d7fdddcb0500972980a9c71c
sakshidulwani <dulwanisakshi@gmail.com>
2021-04-24 21:19:53 +0530

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { Redirect, Route, Switch } from 'react-router';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Home from './Pages/Home';
 import Service from './Pages/Service';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Switch>
      <Route  exact path="/" component={Home} />
      <Route  exact path="/about" component={About} />
