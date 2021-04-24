5e214a2e61c7ed9fd48791798601bdb3c99fef8d
sakshidulwani <dulwanisakshi@gmail.com>
2021-04-24 20:34:46 +0530

    Added Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Home from './Pages/Home';
+import Service from './Pages/Service';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Switch>
+     <Route  exact path="/" component={Home} />
+     <Route  exact path="/about" component={About} />
+     <Route  exact path="/service" component={Service} />
+     <Route  exact path="/contact" component={Contact} />
+     <Redirect to="/"/>
+     </Switch>
+    </>
   );
 }
 
---
+++ src/Pages/Service.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Service() {
+  return (
+    <div >
+     <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Service;
