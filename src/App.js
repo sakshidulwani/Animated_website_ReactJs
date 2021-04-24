dc726ef49ebfdd3e7877741017d2967d86b246b7
sakshidulwani <dulwanisakshi@gmail.com>
2021-04-25 01:47:49 +0530

    Added Css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { Redirect, Route, Switch } from 'react-router';
